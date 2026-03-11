5710863761a83dcf2a659ac9dfd4bdc5f309f6da
Jashan19 <152475739+Jashan19@users.noreply.github.com>
2026-03-11 08:34:51 +0530

    frontend with llm

--- frontend.py
+++ frontend.py
@@ -1,15 +1,22 @@
 import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+load_dotenv()
+client = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
 if "messages" not in st.session_state:
-    st.session_state.messages=[]
+    st.session_state.messages = []
 for role, msg in st.session_state.messages:
     with st.chat_message(role):
         st.write(msg)
-user=st.chat_input(" ")
+user = st.chat_input("Ask something")
 if user:
     st.session_state.messages.append(("user", user))
     with st.chat_message("user"):
         st.write(user)
-    bot_reply="Hello"+user
-    st.session_state.messages.append(("bot_reply", bot_reply))
-    with st.chat_message("bot_reply"):
+    response = client.invoke(user)
+    bot_reply = response.content
+    st.session_state.messages.append(("assistant", bot_reply))
+    with st.chat_message("assistant"):
         st.write(bot_reply)
